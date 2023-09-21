c295d5c8c1839f268da366c053c57f3f0d3e06ae
1Mashilo <christophermashilo@gmail.com>
2023-09-21 15:58:44 -0700

    new updates

--- main.c
+++ main.c
@@ -1,93 +1,46 @@
 #include "shell.h"
 
 /**
- * main - Entry point of the shell.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the arguments.
- *
- * Return: The exit status of the shell.
- */
-int main(int argc, char *argv[])
+* main - entry point
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0 on success, 1 on error
+*/
+int main(int argc, char **argv)
 {
-    char *program_name;
-    int input_fd;
-     int exit_status = 0;
+info_t info[] = { INFO_INIT };
+int file_descriptor = 2;
 
-    if (argc > 0)
-        program_name = argv[0];
+asm ("mov %1, %0\n\t"
+"add $3, %0"
+: "=r" (file_descriptor)
+: "r" (file_descriptor));
 
-    if (argc > 1)
-    {
-        input_fd = open(argv[1], O_RDONLY);
-        if (input_fd == -1)
-        {
-            perror(program_name);
-            return (1);
-        }
-
-        exit_status = execute_shell(input_fd);
-        close(input_fd);
-        return exit_status;
-    }
-    else
-    {
-        return interactive_shell();
-    }
+if (argc == 2)
+{
+file_descriptor = open(argv[1], O_RDONLY);
+if (file_descriptor == -1)
+{
+if (errno == EACCES)
+exit(126);
+if (errno == ENOENT)
+{
+_eputs(argv[0]);
+_eputs(": 0: Can't open ");
+_eputs(argv[1]);
+_eputchar('\n');
+_eputchar(BUF_FLUSH);
+exit(127);
+}
+return (EXIT_FAILURE);
+}
+info->readfd = file_descriptor;
 }
 
-
-/**
- * execute_shell - Executes the shell.
- * @input_fd: The file descriptor for input.
- *
- * Return: The exit status of the shell.
- */
-int execute_shell(int input_fd)
-{
-    int ss_interactive = (input_fd == STDIN_FILENO);
-    int builtin_return = 0;
-    char *command_line = NULL;
-    env_node_t *env_list = NULL;
-     int file_fd;
-
-    env_list = ss_convert_to_env_node_t(environ);
-
-    while (1)
-    {
-        ssize_t read_result;
-        char **parsed_input;
-
-        if (ss_interactive)
-            display_prompt("$ ");
-
-        if (ss_interactive)
-        {
-            file_fd = STDIN_FILENO;
-            read_result = read_command_line(file_fd, &command_line);
-        }
-        else
-        {
-            file_fd = input_fd;
-            read_result = read_command_line(file_fd, &command_line);
-        }
-
-        if (read_result != -1)
-        {
-            parsed_input = ss_parse_input(command_line);
-            
-            if (parsed_input != NULL)
-            {
-                handle_input(parsed_input, env_list);
-                free_parsed_input(parsed_input);
-            }
-            free(command_line);
-        }
-        else if (ss_interactive)
-        {
-            break;
-        }
-    }
-    ss_free_env_list(env_list);
-    return (builtin_return);
+s_populate_env_list(info);
+read_history(info);
+hsh(info, argv);
+return (EXIT_SUCCESS);
 }
 
--- shell.h
+++ shell.h
@@ -1,72 +1,252 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdarg.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <sys/stat.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 
-#define BUF_FLUSH '\0'
-#define MAX_INPUT_SIZE 1024
+/* for read/write buffers */
+#define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* for command chaining */
+#define CMD_NORM 0
+#define CMD_OR 1
+#define CMD_AND 2
+#define CMD_CHAIN 3
+
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE ".simple_shell_history"
+#define HIST_MAX 4096
+#define WRITE_BUFFER_SIZE 1024
+#define BUFFER_FLUSH -1
 extern char **environ;
 
+typedef struct alias {
+    char *name;
+    char *value;
+    struct alias *next;
+} alias_t;
+
+
 /**
- * struct env_node - Represents a node in the environment variable
- * linked list.
- * @data: The data (environment variable) stored in the node.
- * @next: A pointer to the next node in the linked list.
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: points to the next node
  */
-
-typedef struct env_node
+typedef struct liststr
 {
-char *data;
-struct env_node *next;
+    int num;
+    char *str;
+    struct liststr *next;
+} list_t;
+
+/**
+ * struct passinfo - contains pseudo-arguments to pass into a function,
+ * allowing a uniform prototype for a function pointer struct
+ * @history_file: history file path
+ * @history: history linked list
+ * @lineCountFlag: flag for line count
+ * @err_num: the error code for exit()s
+ * @argv: array of strings generated from arguments
+ * @path: current command path
+ * @lineCount: line count
+ * @histcount: history count
+ * @get_environ: environment variables
+ * @status: return status of the last exec'd command
+ * @arguments: additional arguments
+ * @program_name: program name
+ * @executable_path: executable path
+ * @argument_vector: argument vector
+ * @cmd_buf_type: command buffer type
+ * @alias:the  alias node
+ * @arg: a string generated from getline containing arguements
+ * @env: linked list environ 
+ * @environ:custom modified copy of environ fromm LL env 
+ * fname: the program filename
+ * @argc:the argument count
+ * @environment: environment variables
+ * @environment_changed: environment change flag
+ */
+typedef struct passinfo
+{
+    char *history_file;
+    list_t *history;
+    int lineCountFlag;
+    char **argv;
+    int argc;
+    char *arg;
+    char *path;
+    char *fname;
+    int linecount;
+    int histcount;
+    char **environ;
+    int err_num;
+    char **get_environ;
+    int status;
+    char *arguments;
+    char *program_name;
+    char *executable_path;
+    char **argument_vector;
+    int cmd_buf_type;
+    list_t *alias;
+    list_t *env;
+    char **environment;
+    int env_changed;
+    int environment_changed;
+    char **cmd_buf;
+    int readfd;
+} info_t;
+
+#define INFO_INIT \
+{ \
+    NULL,        /* history_file */ \
+    NULL,        /* history */ \
+    0,           /* lineCountFlag */ \
+    NULL,        /* argv */ \
+    0,           /* argc */ \
+    NULL,        /* arg */ \
+    NULL,        /* path */ \
+    NULL,        /* fname */ \
+    0,           /* linecount */ \
+    0,           /* histcount */ \
+    NULL,        /* environ */ \
+    0,           /* err_num */ \
+    NULL,        /* get_environ */ \
+    0,           /* status */ \
+    NULL,        /* arguments */ \
+    NULL,        /* program_name */ \
+    NULL,        /* executable_path */ \
+    NULL,        /* argument_vector */ \
+    0,           /* cmd_buf_type */ \
+    NULL,        /* alias */ \
+    NULL,        /* env */ \
+    NULL,        /* environment */ \
+    0,           /* env_changed */ \
+    0,           /* environment_changed */ \
+    NULL,        /* cmd_buf */ \
+    0            /* readfd */ \
 }
-env_node_t;
-
-int interactive_shell(void);
-
-char *ss_strdup(const char *string);
-char *ss_strconcat(const char *string1, const char *sep, const char *string2);
-char *ss_strcpy(char *dest, const char *src);
-size_t ss_strlen(const char *string);
-int ss_strcmp(const char *string1, const char *string2);
-int ss_strncmp(const char *string1, const char *string2, size_t n);
-void free_parsed_input(char **parsed_input);
-char **env_list_to_char_array(env_node_t *env_list);
-char *ss_strcat(const char *string1, const char *string2);
 
 
-void ss_exit(void);
-int ss_find_builtin(char **argv, env_node_t *env_list);
-void ss_env(env_node_t *env_list);
-int ss_execute_command(char **argv, env_node_t *env_list);
-char *ss_find_command(const char *pathstring, const char *command);
-char *ss_getenv(env_node_t *env_list, const char *var_name);
-env_node_t *ss_convert_to_env_node_t(char **environ);
-size_t ss_print_env_list(env_node_t *env_list);
-void ss_add_to_env_list(env_node_t **env_list, const char *env_var);
+/**
+ * struct builtin - contains a built-in command string and related function
+ * @type: the built-in command string
+ * @func: the associated function
+ */
+typedef struct builtin
+{
+    char *type;
+    int (*func)(info_t *);
+} builtin_table;
 
-int ss_command(const char *file_path);
-size_t count_words(const char *str);
+int hsh(info_t *, char **);
+int loophsh(char **);
+int s_set_alias(info_t *info, char *alias_str);
+int s_print_alias(list_t *node);
+int s_myalias(info_t *info);
+int s_exit(info_t *info);
+int s_cd(info_t *info);
+int s_help(info_t *info);
+int s_history(info_t *info);
+int s_unset_alias(info_t *info, char *alias_str);
+int s_erratoi(char *str);
+void s_print_error(info_t *info, char *error_str);
+int s_print_d(int d, int fd);
+char *s_convert_number(long int num, int base, int flags);
+void s_remove_comments(char *str);
+char *_strdup(const char *str);
+void _puts(char *str);
+int _putchar(char c);
+int _putsfd(char *str, int fd);
+int _putfd(char c, int fd);
+int _eputchar(char c);
+void _eputs(char *str);
+void s_sigint_handler(__attribute__((unused)) int sig_num);
+ssize_t s_input_buffer(info_t *info, char **command_buffer, size_t *buffer_length);
+char *startsWith(const char *haystack, const char *needle);
+char *s_strncpy(char *destination, char *source, int num);
+char *s_strncat(char *destination, char *source, int num);
+int stringCompare(char *s1, char *s2);
+int stringLength(char *s);
+char *stringConcatenate(char *dest, char *src);
+char *s_strchr(char *str, char c);
+char **s_get_environment(info_t *info);
+int _unset_environment_variable(info_t *info, char *var);
+int s_set_environment_variable(info_t *info, char *variable, char *value);
+void initialize_info(info_t *info);
+void populate_info(info_t *info, char **av);
+void free_info(info_t *info, int free_environ);
+ ssize_t s_get_input_line(info_t *info);
+ssize_t s_read_buffer(info_t *info, char *buff, size_t *len);
+ int s_get_line(info_t *info, char **ptr, size_t *length);
+size_t list_len(const list_t *list);
+char **list_to_strings(list_t *head);
+size_t print_list(const list_t *list);
+list_t *node_starts_with(list_t *node, char *prefix, char next_char);
+ssize_t get_node_index(list_t *head, list_t *node);
+list_t *add_node(list_t **list, const char *str, int num);
+list_t *add_node_end(list_t **list, const char *str, int num);
+size_t print_list_str(const list_t *list);
+int delete_node_at_index(list_t **list, unsigned int index);
+void free_list(list_t **list);
+int is_interactive(info_t *info);
+int is_delimiter(char c, char *delimiters);
+int is_alpha(int c);
+int string_to_integer(char *s);
+int find_buitin(info_t *info);
+void find_cmd(info_t *info);
+void fork_cmd(info_t *info);
 
-int execute_shell(int input_fd);
-int handle_input(char **parsed_input, env_node_t *env_list);
-char **ss_parse_input(const char *input);
-char *ss_find_path(const char *pathstring, const char *command);
-void s_builtin(char **parsed_input, env_node_t *env_list, int *builtin_return);
+int _strlen(char *s);
+int _strcmp(char *string1, char *s2);
+char *_strcat(char *destination, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strdup(const char *str);
+int _putchar(char c);
+char **splitString(char *str, char *delimiters);
+int _strlen(char *s);
 
-void display_prompt(const char *prompt);
-ssize_t read_command_line(int file_descriptor, char **output_line);
-void ss_int_string(int num, char *str);
 
-void ss_free_env_list(env_node_t *env_list);
+
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+int isChainDelimiter(info_t *info, char *buf, size_t *p);
+void checkChain(info_t *info, char *buf, size_t *p, size_t i, size_t len);
+int replaceAlias(info_t *info);
+int replaceVars(info_t *info);
+int replaceString(char **old, char *new);
+void *customRealloc(void *previousBlock, unsigned int oldSize, unsigned int newSize);
+void freeStringArray(char **stringArray);
+char *setMemory(char *memoryBlock, char byteValue, unsigned int byteCount);
+int s_env(info_t *info);
+char *s_get_env_value(info_t *info, const char *name);
+int s_mysetenv(info_t *info);
+int s_myunsetenv(info_t *info);
+int s_populate_env_list(info_t *info);
+int isExecutableCommand(info_t *info, char *filePath);
+char *copyCharacters(char *sourceString, int startIndex, int stopIndex);
+char *findExecutablePath(info_t *info, char *pathString, char *command);
+
+
 
 #endif
+
 
